0f8cb5f98f75471ac3c64e5d733bd79733f93cc5
Terry Stewart <tcstewar@uwaterloo.ca>
2014-07-17 19:12:13 -0600

    updated sheep

--- tag/tag_sheep.py
+++ tag/tag_sheep.py
@@ -5,7 +5,7 @@ import numpy as np
 import tag
 
 motor_speed_factor = -0.2 * tag.get_dir()
-sheep_motor_speed_factor=0.4
+sheep_motor_speed_factor=0.5 * tag.get_dir()
 # how fast can the bot go as a maximum in each direction
 input_factor=0.9
 
@@ -23,12 +23,12 @@ with model:
     r2 = nengo.Ensemble(100, dimensions=1, label='r2')
 # replica of what is sent to the motor so that we can see the behavior and allows plotting - this is also what we are probing
     bot1 = nengo_pushbot.PushBotNetwork(tag.get_addr())
-    bot1.laser(tag.get_self_freq())
+    #bot1.laser(tag.get_self_freq())
     bot1.led(tag.get_self_freq())
 
     #bot1.laser(0)
     #bot1.track_freqs([200, 300])
-    bot1.track_freqs([tag.get_good_freq()])
+    bot1.track_freqs([tag.get_good_freq(), tag.get_bad_freq()], certainty_scale=10000)
 
 
     half_size = 64.0
@@ -51,28 +51,42 @@ with model:
     x_limit.append(127.0)
 
 # 3 dimensions are x y and a confidence level for how sure there is something there.
-    pos0 = nengo.Ensemble(100, 3, label='pos0')
+    pos0 = nengo.Ensemble(100, 3, label='pos_good')
 # only want 2 of the dimensions - removing the 3rd component for tracking.
-    nengo.Connection(bot1.tracker_0[:2], pos0[:2])
+    nengo.Connection(bot1.tracker_0, pos0)
+    
+    pos1 = nengo.Ensemble(100, 3, label='pos_bad')
+    nengo.Connection(bot1.tracker_1, pos1)
+
+    select = nengo.Ensemble(600,6, label='select')
+    nengo.Connection(pos0, select[0:3])
+    nengo.Connection(pos1, select[3:6])
 
     def normalize_coord(x):
         return (x-half_size)/half_size
 
 # divides the vision field into 9 positions - says that if the interest area is not in the middle then look up or down and turn either left / right to put the stimuli in the middle. This also allows distance to be maintained.
     def orient(x):
+        """
         if x[0] < normalize_coord(y_limit[1]): #forward/backward
             y_ret = 1
         elif x[0] >= normalize_coord(y_limit[1]) and x[0] < normalize_coord(y_limit[2]):
             y_ret = 0
         else:
             y_ret = -1
-
-        if x[1] < normalize_coord(x_limit[3]): #rotate left/right
-            x_ret = 0.1
-        elif x[1] >= normalize_coord(x_limit[3]) and x[1] <= normalize_coord(x_limit[4]):
-            x_ret = 0
+        """
+        if (x[2] > x[5] and x[2] > 0.2):
+            y_ret = 1
+            if x[1] < normalize_coord(x_limit[3]): #rotate left/right
+                x_ret = 0.07
+            elif x[1] >= normalize_coord(x_limit[3]) and x[1] <= normalize_coord(x_limit[4]):
+                x_ret = 0
+            else:
+                x_ret = -0.12
+            
         else:
-            x_ret = -0.1
+            x_ret = 0.05
+            y_ret = 0
 
         return [y_ret, x_ret]
 
@@ -80,7 +94,7 @@ with model:
     #nengo.Connection(bot1.tracker_1, pos1)
 
 # this is what controls the robot - from control to position through the function orient - transform is just a one to one mapping of a 2D matrix.
-    nengo.Connection(pos0, control, function=orient, transform=[[1,0],[0,1]], synapse=0.002)
+    nengo.Connection(select, control, function=orient, transform=[[1,0],[0,1]], synapse=0.002)
 
 # duplication so that both robots are controlled - one with the keyboard and the other with the stimuli.
     nengo.Connection(control, l1, transform=[[input_factor, 0]])
